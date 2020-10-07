be37f12466488e90fc6774cfbea6544cfae1ffd1
LinuxCat <46047705+L-i-n-u-x-C-a-t@users.noreply.github.com>
2020-10-07 11:36:20 +0200

    Create arbrefoot.py

---
+++ python/arbre/arbrefoot.py
@@ -0,0 +1,4 @@
+from arbresimplifié import Noeud
+
+arbrefoot = Noeud(1, Noeud(2, Noeud(3,"Nantes","Rennes"),Noeud(4,"Paris","Lyon")), Noeud(5, Noeud(6,"Laval","Rouen"),Noeud(7,"Angers","Le Mans")))
+print(arbrefoot)
